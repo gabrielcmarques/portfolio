b7d43b74e84035dd3478f26f6d799154d71c296d
Gabriel C. Marques <gabrielchristomarques@gmail.com>
2022-05-28 23:01:53 -0300

    teste model

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import PerfilHomepage
 
 
 def homePage(request):
-    return render(request, "portfolio_app/homepage.html")
+    perfil = PerfilHomepage.objects.all()
+    context = {'perfil': perfil}
+    return render(request, "portfolio_app/homepage.html", context)
 
 
 def aboutPage(request):
