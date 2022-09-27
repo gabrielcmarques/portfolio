696c8988129b63d5699bf837ec55ce7bc7ce4492
Gabriel <gabrielchristomarques@gmail.com>
2022-09-26 22:01:48 -0300

    estilos_CSS_BS

--- projeto_web_cralwer/forms.py
+++ projeto_web_cralwer/forms.py
@@ -1,8 +1,9 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Artigo
 
-
 class ArtigoForm(ModelForm):
+    url = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'ex: https://github.com/'}))
     class Meta:
         model = Artigo
-        fields = ['titulo', 'url']
+        fields = ['titulo', 'url'] 
